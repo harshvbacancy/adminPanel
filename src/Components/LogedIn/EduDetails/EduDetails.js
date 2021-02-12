7353050da3dd4588173844ecbb7c1db65f607294
harshvbacancy <harsh.verma@bacancy.com>
2021-02-12 15:35:40 +0530

    modified3

--- src/Components/LogedIn/EduDetails/EduDetails.js
+++ src/Components/LogedIn/EduDetails/EduDetails.js
@@ -1,10 +1,53 @@
 import classes from './EduDetails.module.css';
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../../UI/Button/Button';
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
 
 
-const EduDetails = () => {
+class  EduDetails extends Component {
+
+    state = {
+        showModal: false
+    }
+
+    handleEdit = (event, userIndex, eduIndex) => {
+        let localUserInfo = JSON.parse(localStorage.getItem('userInfo'));
+        let user = localUserInfo[userIndex];
+        let edu = user.Reg2[eduIndex];
+        this.setState({ edu });
+        this.setState({ showModal: true, userIndex: userIndex, eduIndex: eduIndex });
+
+    }
+    handleDelete = (event, userIndex, eduIndex) => {
+        let isConfirm = window.confirm('Delete this eduction record?');
+        if (isConfirm) {
+            let values = JSON.parse(localStorage.getItem('userInfo'));
+            let user = values[userIndex];
+
+            user.Reg2.splice(eduIndex, 1);
+            values[userIndex] = user;
+            localStorage.setItem('userInfo', JSON.stringify(values))
+            this.props.history.push('/educationalDetails')
+        }
+    }
+    submitChanges = () => {
+        let values = JSON.parse(localStorage.getItem('userInfo'));
+        let user = values[this.state.userIndex];
+        user.Reg2[this.state.eduIndex] = this.state.edu;
+        values[this.state.userIndex] = user;
+        localStorage.setItem('userInfo', JSON.stringify(values));
+        this.setState({ showModal: false })
+        alert('successfully changed..');
+        this.props.history.push('/educationalDetails');
+    }
+    handleModalChange = (event) => {
+        let edu = this.state.edu;
+        edu[event.target.name] = event.target.value;
+        this.setState({ edu: edu })
+
+    }
+
+    render() {
     let localUserInfo = JSON.parse(localStorage.getItem('userInfo'));
 
     let names = localUserInfo.map(
@@ -24,8 +67,8 @@ const EduDetails = () => {
                 <td>{education.EndDate}</td>
                 <td >
                 
-                    <Button btnType="Success">Edit</Button>
-                    <Button btnType="Danger">Delete</Button>
+                    <Button btnType="Success" clicked={(event) => this.handleEdit(event, localUserInfo.indexOf(user), user.Reg2.indexOf(education))}>Edit</Button>
+                    <Button btnType="Danger" clicked={(event) => this.handleDelete(event, localUserInfo.indexOf(user), user.Reg2.indexOf(education))}>Delete</Button>
                 </td>
             </tr>
 
@@ -44,6 +87,7 @@ const EduDetails = () => {
             </table>
         </div>
     );
+    }
 }
 
 export default EduDetails; 
