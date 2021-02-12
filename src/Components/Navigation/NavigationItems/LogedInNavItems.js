5ed4921cf27f012db9046edbf961ac21a2c7d4e5
harshvbacancy <harsh.verma@bacancy.com>
2021-02-12 13:09:27 +0530

    modified

--- src/Components/Navigation/NavigationItems/LogedInNavItems.js
+++ src/Components/Navigation/NavigationItems/LogedInNavItems.js
@@ -17,34 +17,3 @@ const LogedInNavItems = (props) => {
 };
 
 export default LogedInNavItems;
-// class LogedInNavItems extends Component {
-//     render() {
-
-//     let navButtons;
-//         let infoViewerButtons;
-//         let changePasswordButtons;
-
-//         navButtons = <LogOut logout={this.props.logOut}/>
-//         infoViewerButtons = 
-//             <>
-//                 <NavigationItem link="/Login">Personal Details</NavigationItem>
-//                 <NavigationItem link="/Reg1">Educational Details</NavigationItem>
-//             </>
-
-//             changePasswordButtons=
-//             <NavigationItem link="/changePassword1">Change Password</NavigationItem>
-//         }
-
-//         return (
-//             <nav className={classes.NavigationItems}>
-//                 <div>
-
-//                     {navButtons}
-//                     {infoViewerButtons}
-//                 </div>
-//                <span>{changePasswordLink}</span>
-//                <span className="loggedInAs">{this.props.loginMessage}</span>
-//             </nav>
-//         )
-//         }
-// }
